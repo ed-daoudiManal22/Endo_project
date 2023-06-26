6c9470de4fc3902ef79baed4b626a498b642833e
ManalChailou <chailou.manal@gmail.com>
2023-06-26 16:44:33 +0100

    login is done

--- app/src/main/java/com/example/myapplication/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/myapplication/ForgotPasswordActivity.java
@@ -1,6 +1,65 @@
 package com.example.myapplication;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ForgotPasswordActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+    private EditText emailInput;
+    private Button resetPasswordButton;
+    private TextView backToLoginLink;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgot_password);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        emailInput = findViewById(R.id.emailInput);
+        resetPasswordButton = findViewById(R.id.resetPasswordButton);
+        backToLoginLink = findViewById(R.id.backToLoginLink);
+
+        resetPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailInput.getText().toString().trim();
+                if (email.isEmpty()) {
+                    Toast.makeText(ForgotPasswordActivity.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                } else {
+                    resetPassword(email);
+                }
+            }
+        });
+
+        backToLoginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ForgotPasswordActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void resetPassword(String email) {
+        firebaseAuth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(ForgotPasswordActivity.this, "Password reset email sent", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(ForgotPasswordActivity.this, "Failed to send password reset email", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
--- app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -4,27 +4,86 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
 public class SignUpActivity extends AppCompatActivity {
+    private EditText nameInput;
+    private EditText emailInput;
+    private EditText passwordInput;
+    private EditText confirmPasswordInput;
     private Button signUpButton;
     private TextView loginLink;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_page);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        nameInput = findViewById(R.id.nameInput);
+        emailInput = findViewById(R.id.emailInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        confirmPasswordInput = findViewById(R.id.confirmPasswordInput);
         signUpButton = findViewById(R.id.signUpButton);
         loginLink = findViewById(R.id.loginLink);
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Perform sign up logic here
-                // TODO: Implement sign up functionality
+                String name = nameInput.getText().toString().trim();
+                String email = emailInput.getText().toString().trim();
+                String password = passwordInput.getText().toString().trim();
+                String confirmPassword = confirmPasswordInput.getText().toString().trim();
+
+                if (name.isEmpty()) {
+                    nameInput.setError("Please enter your name");
+                    nameInput.requestFocus();
+                    return;
+                }
+
+                if (email.isEmpty()) {
+                    emailInput.setError("Please enter your email");
+                    emailInput.requestFocus();
+                    return;
+                }
+
+                if (password.isEmpty()) {
+                    passwordInput.setError("Please enter a password");
+                    passwordInput.requestFocus();
+                    return;
+                }
+
+                if (confirmPassword.isEmpty()) {
+                    confirmPasswordInput.setError("Please confirm your password");
+                    confirmPasswordInput.requestFocus();
+                    return;
+                }
+
+                if (!password.equals(confirmPassword)) {
+                    confirmPasswordInput.setError("Passwords do not match");
+                    confirmPasswordInput.requestFocus();
+                    return;
+                }
+
+                signUpUser(name, email, password);
             }
         });
 
@@ -36,5 +95,53 @@ public class SignUpActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void signUpUser(String name, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if (user != null) {
+                                // Update user's display name
+                                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                        .setDisplayName(name)
+                                        .build();
+
+                                user.updateProfile(profileUpdates)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(SignUpActivity.this, "Sign up successful", Toast.LENGTH_SHORT).show();
+                                                    Intent intent = new Intent(SignUpActivity.this, WelcomeActivity.class);
+                                                    startActivity(intent);
+                                                    finish();
+                                                } else {
+                                                    Toast.makeText(SignUpActivity.this, "Failed to update user profile", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                            }
+                        } else {
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthWeakPasswordException e) {
+                                passwordInput.setError("Weak password. Please enter a stronger password.");
+                                passwordInput.requestFocus();
+                            } catch (FirebaseAuthInvalidCredentialsException e) {
+                                emailInput.setError("Invalid email address");
+                                emailInput.requestFocus();
+                            } catch (FirebaseAuthUserCollisionException e) {
+                                emailInput.setError("Email address already in use");
+                                emailInput.requestFocus();
+                            } catch (Exception e) {
+                                Toast.makeText(SignUpActivity.this, "Sign up failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
     }
 }
