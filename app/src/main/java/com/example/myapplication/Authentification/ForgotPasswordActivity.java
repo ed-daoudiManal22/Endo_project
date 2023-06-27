4c09d5b8f0fca856a85a8192f3d1c961f1232c4c
ManalChailou <chailou.manal@gmail.com>
2023-06-27 21:21:41 +0100

    Organization

--- app/src/main/java/com/example/myapplication/Authentification/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/myapplication/Authentification/ForgotPasswordActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.myapplication.Authentification;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.myapplication.R;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
