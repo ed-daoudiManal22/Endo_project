39abfde0aaf0df13600703b04d9132a7ad955414
ed-daoudiManal22 <manaloglu@gmail.com>
2023-06-28 15:11:59 +0100

    modifss

--- app/src/main/java/com/example/myapplication/UserActivity.java
+++ app/src/main/java/com/example/myapplication/UserActivity.java
@@ -2,5 +2,21 @@ package com.example.myapplication;
 
 import android.app.Activity;
 
-public class UserActivity extends Activity {
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserActivity extends AppCompatActivity {
+
+
+
 }
