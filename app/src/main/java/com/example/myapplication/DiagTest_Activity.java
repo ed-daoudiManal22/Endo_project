cf166a810ff0b13555463f2dc9181c0d0916f678
ManalChailou <chailou.manal@gmail.com>
2023-07-13 17:25:24 +0100

    updated

---
+++ app/src/main/java/com/example/myapplication/DiagTest_Activity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+public class DiagTest_Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.score);
+    }
+}
