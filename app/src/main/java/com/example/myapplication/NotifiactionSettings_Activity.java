bad244ae2d224e39c0d348a622d1ba7eb0773add
ManalChailou <chailou.manal@gmail.com>
2023-08-13 13:42:28 +0100

    Updated

--- app/src/main/java/com/example/myapplication/NotifiactionSettings_Activity.java
+++ app/src/main/java/com/example/myapplication/NotifiactionSettings_Activity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,18 +18,23 @@ import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ListenerRegistration;
 
 import java.util.Calendar;
 
 public class NotifiactionSettings_Activity extends AppCompatActivity {
     PendingIntent pending_intent;
     AlarmManager alarm_manager;
+    private ImageView leftIcon;
+    private boolean isCommunityNotificationOn = false;
+    private ListenerRegistration blogsListenerRegistration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notification_settings);
+        leftIcon = findViewById(R.id.leftIcon);
 
         notification_channel();
         pending_intent = PendingIntent.getBroadcast(getApplicationContext(), 0, new Intent(this, NotificationReceiver.class), PendingIntent.FLAG_IMMUTABLE);
@@ -40,28 +47,31 @@ public class NotifiactionSettings_Activity extends AppCompatActivity {
         // Assuming you have a reference to your Firestore collection "Blogs"
         CollectionReference blogsCollection = FirebaseFirestore.getInstance().collection("Blogs");
 
-        CommunityNotif.setOnClickListener(e->{
-            if (CommunityNotif.isChecked()) {
-                // Community notifications are turned on
-                // Add a snapshot listener to monitor for new blogs
-                blogsCollection.addSnapshotListener((value, error) -> {
-                    if (error != null) {
-                        Log.e("Firestore Listener", "Error listening for new blogs", error);
-                        return;
+        // Set up a snapshot listener for "Blogs" collection
+        blogsListenerRegistration = blogsCollection.addSnapshotListener((value, error) -> {
+            if (error != null) {
+                Log.e("Firestore Listener", "Error listening for new blogs", error);
+                return;
+            }
+
+            if (isCommunityNotificationOn) {
+                for (DocumentChange dc : value.getDocumentChanges()) {
+                    if (dc.getType() == DocumentChange.Type.ADDED) {
+                        // A new blog has been added, trigger the notification
+                        set_notification_alarm(0, "New Blog Added", "A member has added a new blog");
+                        // Show a toast message
+                        Toast.makeText(NotifiactionSettings_Activity.this, "Community notifications ON", Toast.LENGTH_SHORT).show();
+                        // Exit the loop after the first added document is found
+                        break;
                     }
-                    for (DocumentChange dc : value.getDocumentChanges()) {
-                        if (dc.getType() == DocumentChange.Type.ADDED) {
-                            // A new blog has been added, trigger the notification
-                            set_notification_alarm(0, "New Blog Added", "A member has added a new blog");
-                            // Show a toast message
-                            Toast.makeText(NotifiactionSettings_Activity.this, "Community notifications ON", Toast.LENGTH_SHORT).show();
-                            // Exit the loop after the first added document is found
-                            break;
-                        }
-                    }
-                });
-            }  else {
-                // Community notifications are turned off
+                }
+            }
+        });
+
+        // CommunityNotif switch click listener
+        CommunityNotif.setOnClickListener(e -> {
+            isCommunityNotificationOn = CommunityNotif.isChecked();
+            if (!isCommunityNotificationOn) {
                 cancel_notification_alarm();
                 // Show a toast message
                 Toast.makeText(NotifiactionSettings_Activity.this, "Community notifications OFF", Toast.LENGTH_SHORT).show();
@@ -70,7 +80,7 @@ public class NotifiactionSettings_Activity extends AppCompatActivity {
         TestNotif.setOnClickListener(e->{
             if (TestNotif.isChecked()) {
                 // Set the alarm to trigger once a month (30 days) with custom name and description
-                set_notification_alarm(30 * 24 * 60 * 60 * 1000, "Test Reminder", "Take diagnostic test");
+                set_test_notification_alarm(30 * 24 * 60 * 60 * 1000, "Test Reminder", "Take diagnostic test");
                 // Show a toast message
                 Toast.makeText(NotifiactionSettings_Activity.this, "Test notifications ON", Toast.LENGTH_SHORT).show();
             } else {
@@ -82,7 +92,7 @@ public class NotifiactionSettings_Activity extends AppCompatActivity {
         });
         test.setOnClickListener(e->{
             if (test.isChecked()) {
-                set_notification_alarm(60 * 1000, "Test Reminder", "Test desc");
+                set_test_notification_alarm (60 * 1000, "Test Reminder", "Test desc");
                 // Show a toast message
                 Toast.makeText(NotifiactionSettings_Activity.this, "Test notifications ON", Toast.LENGTH_SHORT).show();
             } else {
@@ -92,7 +102,15 @@ public class NotifiactionSettings_Activity extends AppCompatActivity {
                 Toast.makeText(NotifiactionSettings_Activity.this, "Test notifications OFF", Toast.LENGTH_SHORT).show();
             }
         });
-
+        leftIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click event, navigate to HelloActivity
+                Intent intent = new Intent(NotifiactionSettings_Activity.this, HomeActivity.class);
+                startActivity(intent);
+                finish(); // Optional: Close the current activity after navigating
+            }
+        });
     }
 
     private void notification_channel() {
@@ -132,8 +150,36 @@ public class NotifiactionSettings_Activity extends AppCompatActivity {
             notificationManager.createNotificationChannel(channel);
         }
     }
+    @Override
+    protected void onDestroy() {
+        // Unregister the snapshot listener to avoid memory leaks
+        if (blogsListenerRegistration != null) {
+            blogsListenerRegistration.remove();
+        }
+        super.onDestroy();
+    }
 
     public void cancel_notification_alarm() {
         alarm_manager.cancel(pending_intent);
     }
+    public void set_test_notification_alarm(long intervalMillis, String name, String description) {
+        long triggerAtMillis = System.currentTimeMillis() + intervalMillis;
+
+        Intent notificationIntent = new Intent(this, NotificationReceiver.class);
+        notificationIntent.putExtra("name", name);
+        notificationIntent.putExtra("description", description);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        alarm_manager.setRepeating(AlarmManager.RTC_WAKEUP, triggerAtMillis, intervalMillis, pendingIntent);
+
+        // Create a notification channel with the specified name and description
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("Notification", "Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Reminder notifications");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 }
