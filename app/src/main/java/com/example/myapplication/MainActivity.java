e24c6b3fcf112f753a43545ede8799b7856da951
ManalChailou <chailou.manal@gmail.com>
2023-08-14 22:37:58 +0100

    Splash animation added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,60 +1,200 @@
 package com.example.myapplication;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.myapplication.Authentification.Authentification;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-//import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
-    private Button startJourneyButton;
-    private Button circularButton;
-    private static final String TAG = "MainActivity";
 
+    private TextView appTitle, appName;
+    private ImageView logo;
+    private View topView1, topView2, topView3;
+    private View bottomView1, bottomView2, bottomView3;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
         setContentView(R.layout.activity_main);
 
-        startJourneyButton = findViewById(R.id.startJourneyButton);
+        appTitle = findViewById(R.id.appTitle);
+        appName = findViewById(R.id.appName);
 
-        startJourneyButton.setOnClickListener(new View.OnClickListener() {
+
+        logo = findViewById(R.id.logo);
+
+        topView1 = findViewById(R.id.topView1);
+        topView2 = findViewById(R.id.topView2);
+        topView3 = findViewById(R.id.topView3);
+
+        bottomView1 = findViewById(R.id.bottomView1);
+        bottomView2 = findViewById(R.id.bottomView2);
+        bottomView3 = findViewById(R.id.bottomView3);
+
+        Animation logoAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.zoom_animation);
+        Animation titleAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.zoom_animation);
+        Animation nameAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.zoom_animation);
+
+        Animation topView1Animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.top_views_animation);
+        Animation topView2Animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.top_views_animation);
+        Animation topView3Animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.top_views_animation);
+
+        Animation bottomView1Animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.bottom_views_animation);
+        Animation bottomView2Animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.bottom_views_animation);
+        Animation bottomView3Animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.bottom_views_animation);
+
+        topView1.startAnimation(topView1Animation);
+        bottomView1.startAnimation(bottomView1Animation);
+
+        topView1Animation.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void onClick(View v) {
-                // Handle the "Get Started" button click event
-                startActivity(new Intent(MainActivity.this, Authentification.class));
-                finish();
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                topView2.setVisibility(View.VISIBLE);
+                bottomView2.setVisibility(View.VISIBLE);
+
+                topView2.startAnimation(topView2Animation);
+                bottomView2.startAnimation(bottomView2Animation);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
             }
         });
 
-        /*FirebaseMessaging.getInstance().getToken()
-                .addOnCompleteListener(new OnCompleteListener<String>() {
+        topView2Animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                topView3.setVisibility(View.VISIBLE);
+                bottomView3.setVisibility(View.VISIBLE);
+
+                topView3.startAnimation(topView3Animation);
+                bottomView3.startAnimation(bottomView3Animation);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        topView3Animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                logo.setVisibility(View.VISIBLE);
+                logo.startAnimation(logoAnimation);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        logoAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                appTitle.setVisibility(View.VISIBLE);
+                appTitle.startAnimation(titleAnimation);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        titleAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                appName.setVisibility(View.VISIBLE);
+                final String animateTxt = appName.getText().toString();
+                appName.setText("");
+                count = 0;
+
+                new CountDownTimer(animateTxt.length() * 100, 100) {
                     @Override
-                    public void onComplete(@NonNull Task<String> task) {
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
-                            return;
-                        }
+                    public void onTick(long millisUntilFinished) {
 
-                        // Get new FCM registration token
-                        String token = task.getResult();
+                        appName.setText(appName.getText().toString()+animateTxt.charAt(count));
+                        count++;
 
-                        // Log and toast
-                        String msg = "FCM Token:"+token;
-                        Log.d(TAG, msg);
-                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
                     }
-                });*/
-    }
 
+                    @Override
+                    public void onFinish() {
+                        // All animations are finished, wait for 3 seconds before redirecting
+                        Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                Intent intent = new Intent(MainActivity.this, Authentification.class);
+                                startActivity(intent);
+                                finish(); // Optionally, you can finish the current activity to prevent going back
+                            }
+                        }, 500); // 1000 milliseconds = 1 second
+                    }
+                }.start();
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
 }
